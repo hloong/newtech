30dacf9c4c394bb190139863ff5ff33fd9d851f9
hloong <hbxglong@163.com>
2017-07-04 13:36:49 +0800

    新技术demo，Rx系列

--- app/src/main/java/com/hloong/newtech/MainActivity.java
+++ app/src/main/java/com/hloong/newtech/MainActivity.java
@@ -30,6 +30,7 @@ public class MainActivity extends Activity {
                 e.onNext("test1");
                 e.onNext("test2");
                 e.onNext("test3");
+
                 e.onComplete();
                 e.onError(new Throwable("onError-->"));
             }
