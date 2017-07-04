644b2d7f0c7028a8afda2f754a4a670d54a5a6b2
hloong <hbxglong@163.com>
2017-07-04 11:47:55 +0800

    新技术demo，Rx系列

---
+++ app/src/main/java/com/hloong/newtech/MainActivity.java
@@ -0,0 +1,62 @@
+package com.hloong.newtech;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+import io.reactivex.Observable;
+import io.reactivex.ObservableEmitter;
+import io.reactivex.ObservableOnSubscribe;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+import static android.content.ContentValues.TAG;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initObservable();
+    }
+
+    private void initObservable() {
+        //创建被观察者
+        Observable<String> observable = Observable.create(new ObservableOnSubscribe<String>() {
+            @Override
+            public void subscribe(ObservableEmitter<String> e) throws Exception {
+                e.onNext("test1");
+                e.onNext("test2");
+                e.onNext("test3");
+                e.onComplete();
+                e.onError(new Throwable("onError-->"));
+            }
+        });
+        //创建观察者
+        Observer<String> observer = new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG,"subscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG,s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG,e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e(TAG,"onCompleted");
+            }
+        };
+        //注册，将观察者和被观察者关联，将会出发OnSubscribe.call方法
+        observable.subscribe(observer);
+    }
+}
